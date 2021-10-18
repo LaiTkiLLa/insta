1fbc06ac3fdec0e9010b4814a26f54754cd925f5
LaiTkiLLa <84670285+LaiTkiLLa@users.noreply.github.com>
2021-10-18 18:02:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,52 @@
 import requests
-from googleapiclient.http import MediaFileUpload
-from Google import Create_service
-
-secret_file = 'credentials.json'
-API_NAME = 'drive'
-API_VERSION = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/drive/']
-
-service = Create_service(secret_file, API_NAME, API_VERSION, SCOPES)
-
-
+import json
 
 METHOD = 'photos.get'
-TOKEN = input('Введите токен:\n')
+TOKEN = input('Введите токен\n')
 V = '5.131'
-id = input('Введите id пользователя:\n')
+id = input('Введите ваш id:\n')
+YA_Token = input('Введите токен яндекса:\n')
 url_vk = 'https://api.vk.com/method/'+METHOD+'?PARAMS&access_token='+TOKEN+'&v='+V
 params_vk = {'owner_id' : id, 'album_id' : 'profile', 'extended' : '1'}
 url_ya = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-headers = {"Authorization": "AQAAAAAgsrE-AADLW8se8WiLSEWUhO9H3-tfWZM"}
-headers_google = {"Authorization": "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"}
-url_google = 'https://www.googleapis.com/upload/drive/v3/files?uploadType=media'
-
-#oauth google 593144244514-k0mub33ahqo1f4f58ol1gr2m5rf26frc.apps.googleusercontent.com
-#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
-
-# Google dz-netology 593144244514
-# drive-api@dz-netology.iam.gserviceaccount.com
-# 105146919918360586871
-# 150421694
-# 89649cef89649cef89649cefc9891dc1258896489649cefe820d12a2a7204186f074e23
-#key = AIzaSyAhZduSivLeo0KK8PFg-8UubZLOZHJmC-E
+headers = {"Authorization": YA_Token}
 
 def photo_link():
     res = requests.get(url=url_vk, params=params_vk)
     response = res.json()['response']['items']
-    print(response)
+    # print(response)
     for a in response:
+        size = a['sizes'][-1]['type']
         likes = a['likes']['count']
         download_url = a['sizes'][-1]['url']
         params_ya = {'path': likes, 'overwrite': 'true', 'url': download_url}
-        # resp = requests.post(url=url_ya, headers=headers, params=params_ya)
-        # print(resp.json())
-        mime_types = ['image/jpeg']
-        file_metadata = {'name' : likes}
+        resp = requests.post(url=url_ya, headers=headers, params=params_ya)
+        print(resp.json())
 
 
-
-
+        json_file = {}
+        json_file['filename'] = likes
+        json_file['size'] = size
+        with open('final.json', 'w') as file:
+            json.dump(json_file,file)
 
 print(photo_link())
 
+
+fields = 'id,media_url'
+access_token = input('Введите долгосрочный токен:\n')
+media_id = input('Введите user_id\n')
+url_insta = 'https://graph.instagram.com/me/media?fields='+fields+'&access_token='+access_token
+
+def photo_link_insta():
+    res = requests.get(url=url_insta)
+    response = res.json()['data']
+    # print(res.json())
+    for media in response:
+        download_url = media['media_url']
+        name = media['id']
+        params_insta = {'path': name, 'overwrite': 'true', 'url': download_url}
+        resp = requests.post(url=url_ya, headers=headers, params=params_insta)
+        print(resp.json())
+
+print(photo_link_insta())
